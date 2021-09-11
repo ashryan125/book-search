9569bf0cae227ab57f5fc3c1643fb2b9455fc58a
ashryan <20115553+ashryan125@users.noreply.github.com>
2021-09-11 14:23:23 -0400

    mutations for SAVE_BOOK and REMOVE_BOOK'

--- Develop/client/src/pages/SearchBooks.js
+++ Develop/client/src/pages/SearchBooks.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
-import { SAVE_BOOK } from '../utils/mutations';
+import { SAVE_BOOK } from '../utils/mutations.js';
 import { useMutation } from '@apollo/client';
-import {QUERY_BOOKS, GET_ME} from '../utils/queries';
+// import {QUERY_BOOKS, GET_ME} from '../utils/queries';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
