45ceb7566a6abe785addd79c0ddd80070bdee9b2
ashryan <20115553+ashryan125@users.noreply.github.com>
2021-09-11 14:03:35 -0400

    updated SavedBooks file to useQuery instead of useEffect on userdata. Updated handleDeleteBooks to use mutations

---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,36 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql `
+ me {
+     _id
+     username
+     email
+     bookCount
+     savedBooks {
+         bookId
+         authors
+         description
+         image
+         link
+         title
+     }
+ }
+`;
+
+// export const QUERY_THOUGHTS = gql`
+//   query thoughts($username: String) {
+//     thoughts(username: $username) {
+//       _id
+//       thoughtText
+//       createdAt
+//       username
+//       reactionCount
+//       reactions {
+//         _id
+//         createdAt
+//         username
+//         reactionBody
+//       }
+//     }
+//   }
+// `;
